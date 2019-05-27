9fbb9e8c1f49288c23aca73edccad0fb6887f681
Vladimir Sitnikov <sitnikov.vladimir@gmail.com>
2019-05-27 22:25:55 +0300

    Rename trunk->master, remove test* tags

--- run.sh
+++ run.sh
@@ -16,6 +16,8 @@ if [ ! -d target/jmeter_git ]; then
 
   # Set 'origin' URL to avoid accidental push to the main repository
   (cd target/jmeter_git; git remote set-url origin https://github.com/vlsi/jmeter-git-cleanup-result.git)
+  # Rename trunk -> master
+  (cd target/jmeter_git; git branch -m trunk master)
 fi
 
 if [ ! -f target/remove_pull ]; then
@@ -28,6 +30,12 @@ fi
 if [ ! -f target/remove_remote_tags ]; then
   echo Removing refs/remotes/tags
   (cd target/jmeter_git; git for-each-ref --format='%(refname)' 'refs/remotes/tags/' | xargs -n1 git update-ref -d)
+  # tags/test-bin has unrelated Git history and its own "Initial commit". We don't need that
+  (cd target/jmeter_git; git update-ref -d refs/tags/test)
+  (cd target/jmeter_git; git update-ref -d refs/tags/test-bin)
+  (cd target/jmeter_git; git update-ref -d refs/tags/test1157668)
+  (cd target/jmeter_git; git update-ref -d refs/tags/testhead)
+
   rm -rf target/jmeter_git/refs/tags
   touch target/remove_remote_tags
 fi
@@ -38,11 +46,11 @@ if [ ! -f target/remove_at_refs ]; then
   touch target/remove_at_refs
 fi
 
-if [ ! -f target/remove_doc_branches ]; then
-  echo Removing refs with @ in name
-  (cd target/jmeter_git; git for-each-ref --format='%(refname)' | grep '/docs' | xargs -n1 git update-ref -d)
-  touch target/remove_doc_branches
-fi
+#if [ ! -f target/remove_doc_branches ]; then
+#  echo Removing refs with @ in name
+#  (cd target/jmeter_git; git for-each-ref --format='%(refname)' | grep '/docs' | xargs -n1 git update-ref -d)
+#  touch target/remove_doc_branches
+#fi
 
 if [ ! -f target/remove_jars ]; then
   if [ ! -d lib ]; then
@@ -56,6 +64,7 @@ if [ ! -f target/remove_jars ]; then
   touch target/remove_jars
 fi
 
+: <<'####.comment'
 if [ -f ref_map.txt ]; then
   if [ ! -f target/update_refs ]; then
     echo Updating tags
@@ -65,6 +74,7 @@ if [ -f ref_map.txt ]; then
 else
   (cd target/jmeter_git; git for-each-ref --format='update %(refname) %(objectname)' 'refs/tags/' > ../../ref_map.txt)
 fi
+####.comment
 
 if [ ! -f target/remove_docs ]; then
   echo Searching for docs/
@@ -82,6 +92,26 @@ if [ -f target/remove_docs_api ]; then
   touch target/remove_docs_api
 fi
 
+: <<'####.comment'
+if [ -f target/remove_changes ]; then
+  echo Searching for xdocs/changes
+  (cd target/jmeter_git; git rev-list --all --objects | grep -E '^\w+ xdocs/changes' | cut -d" " -f1 > ../to-delete.txt)
+  echo Removing xdocs/changes
+  (cd target; java -jar ../lib/bfg.jar --no-blob-protection --strip-blobs-with-ids ./to-delete.txt jmeter_git)
+  touch target/remove_changes
+fi
+
+if [ -f target/remove_comp_ref ]; then
+  echo Searching for xdocs/usermanual/component_reference.xml
+  (cd target/jmeter_git; git rev-list --all --objects | grep -E '^\w+ xdocs/usermanual/component_reference.xml' | cut -d" " -f1 > ../to-delete.txt)
+  echo Removing xdocs/usermanual/component_reference.xml
+  (cd target; java -jar ../lib/bfg.jar --no-blob-protection --strip-blobs-with-ids ./to-delete.txt jmeter_git)
+  touch target/remove_comp_ref
+fi
+####.comment
+
 (cd target/jmeter_git; git reflog expire --expire=now --all && git gc --prune=now --aggressive)
 
 du -h target/jmeter_git
+
+#./roundtrip.sh
